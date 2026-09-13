b39bc39a59094dd83a6e75500135d8305b5e18b3
Praiciya <praiciya.j@gmail.com>
2026-09-13 19:47:21 +0530

    Request and response key methods and properties

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const http = require('http');
 
 //creating server
-const server = http.createServer((req, resp) => {
+const server = http.createServer((req, res) => {
     console.log("Server created")
+
+    //request key properties
+    console.log(req.url)
+    console.log(req.method)
+    console.log(req.headers)
+    //returing as plain text
+    // res.setHeader('Content-Type', "text/plain")
+    // res.write("Welcome to the page")
+
+    //returing as html
+        res.setHeader('Content-Type', "text/html")
+    res.write("<h3>Welcome to the page</h3>")
+    res.end()
 })
 
 //listen to server
